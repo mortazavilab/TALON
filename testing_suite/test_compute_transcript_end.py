cb9f2e9f83ced9baae0317bd5766d46a4c2a7a3e
Dana Elizabeth Wyman <dwyman@compute-1-13.local>
2018-04-09 13:32:39 -0700

    Created my first test function for TALON. Tests end computation for sam transcripts against hand-verified examples

---
+++ testing_suite/test_compute_transcript_end.py
@@ -0,0 +1,38 @@
+import pytest
+import sys
+sys.path.append("..")
+from sam_transcript import *
+
+class TestComputeTranscriptEnd(object):
+    def test_MN_Only(self):
+        """ This example (from transcript c3098/f3p2/3199 in 
+            GM12878_chr1_clean.sam) contains only M and N operations. The 
+            transcript is on the minus strand, but the sequence and CIGAR in 
+            BAM are relative to the forward strand.
+        """
+        start = 1081827
+        cigar = "2557M97N26M1371N135M1126N66M297N96M2755N" + \
+                "76M1043N94M425N113M23956N38M"
+        assert compute_transcript_end(start, cigar) == 1116097
+
+    def test_deletion(self):
+        """ This example (from transcript c2986/f14p15/2748 in 
+            GM12878_chr1_clean.sam) contains M, N, and D operations. This is a 
+            plus strand transcript. 
+        """
+        start = 203305518
+        cigar = "231M1355N1013M1D1504M"
+
+        assert compute_transcript_end(start, cigar) == 203309621
+
+    def test_softClipping(self):
+        """ This example (from transcript c28239/f1p4/3076 in
+            GM12878_chr1_clean.sam) contains S operations. This is a
+            plus strand transcript.
+        """
+        start = 167936402
+        cigar = "136S114M15291N55M14767N93M8108N186M12479N114M3595N136M" + \
+                "1886N215M9041N94M1294N120M543N261M33577N118M4536N116M" + \
+                "1444N87M228N328M18393N139M1830N157M630N89M1892N106M6907N420M"
+        
+        assert compute_transcript_end(start, cigar) == 168075790
