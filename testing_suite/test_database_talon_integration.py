7cb4423f54fbe7c46ee6b90e1a2a41e1cf91cb18
Dana Elizabeth Wyman <dwyman@compute-4-47.local>
2018-11-15 21:12:35 -0800

    Added second TALON run on same test transcript, including a validation of the counters after this run

--- testing_suite/test_database_talon_integration.py
+++ testing_suite/test_database_talon_integration.py
@@ -4,6 +4,10 @@
 
 import pytest
 import subprocess
+import sqlite3
+import sys
+sys.path.append("..")
+import talon as TALON
 @pytest.mark.integration
 
 class TestDatabaseTalonIntegration(object):
@@ -53,7 +57,7 @@ class TestDatabaseTalonIntegration(object):
                 line = line.strip().split("\t")
 
                 # From the header, figure out which columns contain the assigned 
-                # gene and transcript IDs, as well as 3' and 5' end differences
+                # gene and transcript IDs
                 if line_num == 0:
                     gene_id_col_index = line.index("gene_id")
                     transcript_id_col_index = line.index("transcript_id")
@@ -65,4 +69,38 @@ class TestDatabaseTalonIntegration(object):
                     assert line[transcript_id_col_index] == "1"
 
                 line_num += 1
-                
+
+    @pytest.mark.incremental
+    def test_TALON_second_simple_run(self):
+        """ After running TALON on the KRT17 example, run the same transcript
+            through under a different dataset name. """ 
+
+        try:
+             subprocess.check_output(
+                     ["python", "../talon.py",
+                      "--f", "input_files/KRT17_test_case/trial2_config.csv",
+                      "-a", "scratch/KRT17.db",
+                      "-b", "hg38",
+                      "--o", "scratch/trial2"])
+        except:
+            pytest.fail("TALON trial 2 failed on KRT17 test")
+
+    @pytest.mark.incremental
+    def test_TALON_second_simple_run_check_counters(self): 
+        """ After running TALON twice on the same input sam file (different
+            dataset label), check whether the counters match the values we 
+            expect. """
+
+        conn = sqlite3.connect("scratch/KRT17.db")
+        cursor = conn.cursor()
+        counter = TALON.get_counters(cursor) 
+
+        assert counter["genes"] == 1
+        assert counter["transcripts"] == 1
+        assert counter["vertices"] == 16
+        assert counter["edges"] == 15
+        assert counter["datasets"] == 2
+        assert counter["observed_starts"] == 2
+        assert counter["observed_ends"] == 2
+
+ 
