8b5b3abce2b1ab2a4c4a89163d32eb442bb64f2a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-09-13 10:50:20 +0200

    remove python 3.6 from the descriptors

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
