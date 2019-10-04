2e7a4c191b5bc84773172e0623c3eba4c7454570
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-10-04 11:33:11 -0700

    Added test case for read partition

---
+++ testing_suite/test_partition_reads.py
@@ -0,0 +1,26 @@
+import pytest
+import pysam
+from talon import process_sams as procsam
+@pytest.mark.unit
+
+class TestPartitionReads(object):
+    def test_partition_reads(self):
+        """ Given a set of toy transcripts, find the minimum number of
+            non-overlapping intervals to contain them. 
+            Expected read groups:
+            - read_1 and read_2 (they are in interval chr1:1-1000)
+            - read_3 (interval chr2:1-100)
+        """
+
+        sams = ["input_files/toy_transcript/toy_reads_for_partition_test.sam"]
+        datasets = ["dataset1"]
+        tmp_dir = "scratch/test_read_partition/"
+
+        read_groups = procsam.partition_reads(sams, datasets, tmp_dir = tmp_dir)
+
+        # Check length and membership of read groups
+        assert len(read_groups) == 2 
+
+        assert [ entry.query_name for entry in read_groups[0] ] == ["read_1", "read_2"]
+        assert [ entry.query_name for entry in read_groups[1] ] == ["read_3"]
+        
