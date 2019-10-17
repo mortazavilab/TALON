3c7c1e7de0d503d5c4a1f1eeb13e9183fe7668cd
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-10-17 15:09:05 -0700

    Made compatible with RunInfo changes

--- testing_suite/test_search_for_overlap_with_gene.py
+++ testing_suite/test_search_for_overlap_with_gene.py
@@ -1,5 +1,6 @@
 import pytest
 from talon import talon
+from talon import init_refs
 from .helper_fns import fetch_correct_ID, get_db_cursor
 @pytest.mark.dbunit
 
@@ -10,9 +11,11 @@ class TestSearchForOverlapWithGene(object):
         """
         conn, cursor = get_db_cursor()
         build = "toy_build"
-        talon.make_temp_novel_gene_table(cursor, "toy_build")
-        location_dict = talon.make_location_dict(build, cursor)
-        run_info = talon.init_run_info(cursor, build)
+        database = "scratch/toy.db"
+        run_info = talon.init_run_info(database, build, tmp_dir = "scratch/tmp/")
+        init_refs.make_temp_novel_gene_table(cursor, "toy_build")
+        location_dict = init_refs.make_location_dict(build, cursor)
+        run_info = talon.init_run_info(database, build, tmp_dir = "scratch/tmp/")
 
         chrom = "chr1"
         pos = [3000, 4000]
@@ -20,24 +23,27 @@ class TestSearchForOverlapWithGene(object):
         gene_ID, match_strand = talon.search_for_overlap_with_gene(chrom, pos[0],
                                                                    pos[1],
                                                                    strand, cursor,
-                                                                   run_info)
+                                                                   run_info, 
+                                                                   "temp_gene")
         assert gene_ID == None
 
         # Should get same results for flipped interval
         gene_ID, match_strand = talon.search_for_overlap_with_gene(chrom, pos[0],
                                                                    pos[1],
                                                                    strand, cursor,
-                                                                   run_info)
+                                                                   run_info, 
+                                                                   "temp_gene")
         assert gene_ID == None
         conn.close()
 
     def test_single_match(self):
         """ Example where the interval overlaps exactly one gene """
         conn, cursor = get_db_cursor()
+        database = "scratch/toy.db"
         build = "toy_build"
-        talon.make_temp_novel_gene_table(cursor, "toy_build")
-        location_dict = talon.make_location_dict(build, cursor)
-        run_info = talon.init_run_info(cursor, build)
+        init_refs.make_temp_novel_gene_table(cursor, "toy_build")
+        location_dict = init_refs.make_location_dict(build, cursor)
+        run_info = talon.init_run_info(database, build, tmp_dir = "scratch/tmp/")
 
         chrom = "chr1"
         pos = [0, 1500]
@@ -46,7 +52,8 @@ class TestSearchForOverlapWithGene(object):
         gene_ID, match_strand = talon.search_for_overlap_with_gene(chrom, pos[0],
                                                                    pos[1],
                                                                    strand, cursor,
-                                                                   run_info)
+                                                                   run_info,
+                                                                   "temp_gene")
 
 
         assert gene_ID == fetch_correct_ID("TG1", "gene", cursor)
@@ -57,11 +64,12 @@ class TestSearchForOverlapWithGene(object):
         """ Example where interval overlaps two genes, one of which is on the 
             same strand. """
         
+        database = "scratch/toy.db"
         conn, cursor = get_db_cursor()
         build = "toy_build"
-        talon.make_temp_novel_gene_table(cursor, "toy_build")
-        location_dict = talon.make_location_dict(build, cursor)
-        run_info = talon.init_run_info(cursor, build)
+        init_refs.make_temp_novel_gene_table(cursor, "toy_build")
+        location_dict = init_refs.make_location_dict(build, cursor)
+        run_info = talon.init_run_info(database, build)
 
         chrom = "chr1"
         pos = [1500, 910]
@@ -70,7 +78,8 @@ class TestSearchForOverlapWithGene(object):
         gene_ID, match_strand = talon.search_for_overlap_with_gene(chrom, pos[0], 
                                                                    pos[1],
                                                                    strand, cursor, 
-                                                                   run_info)
+                                                                   run_info,
+                                                                   "temp_gene")
 
         assert gene_ID == fetch_correct_ID("TG3", "gene", cursor)
         assert match_strand == strand
@@ -80,11 +89,12 @@ class TestSearchForOverlapWithGene(object):
         """ Example where the overlap is on the same strand. Query start is to 
             the left of the gene, and query end is before the end of the gene. """
 
+        database = "scratch/toy.db"
         conn, cursor = get_db_cursor()
         build = "toy_build"
-        talon.make_temp_novel_gene_table(cursor, "toy_build")
-        location_dict = talon.make_location_dict(build, cursor)
-        run_info = talon.init_run_info(cursor, build)
+        init_refs.make_temp_novel_gene_table(cursor, "toy_build")
+        location_dict = init_refs.make_location_dict(build, cursor)
+        run_info = talon.init_run_info(database, build)
 
         chrom = "chr1"
         pos = [550, 1700]
@@ -93,7 +103,8 @@ class TestSearchForOverlapWithGene(object):
         gene_ID, match_strand = talon.search_for_overlap_with_gene(chrom, pos[0],
                                                                    pos[1],
                                                                    strand, cursor,
-                                                                   run_info)
+                                                                   run_info,
+                                                                   "temp_gene")
 
         assert gene_ID == fetch_correct_ID("TG3", "gene", cursor)
         assert match_strand == strand
@@ -103,11 +114,12 @@ class TestSearchForOverlapWithGene(object):
         """ Example where interval overlaps one gene in the antisense direction.
         """
 
+        database = "scratch/toy.db"
         conn, cursor = get_db_cursor()
         build = "toy_build"
-        talon.make_temp_novel_gene_table(cursor, "toy_build")
-        location_dict = talon.make_location_dict(build, cursor)
-        run_info = talon.init_run_info(cursor, build)
+        init_refs.make_temp_novel_gene_table(cursor, "toy_build")
+        location_dict = init_refs.make_location_dict(build, cursor)
+        run_info = talon.init_run_info(database, build)
 
         chrom = "chr1"
         pos = [1400, 2100]
@@ -116,7 +128,8 @@ class TestSearchForOverlapWithGene(object):
         gene_ID, match_strand = talon.search_for_overlap_with_gene(chrom, pos[0],
                                                                    pos[1],
                                                                    strand, cursor,
-                                                                   run_info)
+                                                                   run_info,
+                                                                   "temp_gene")
 
         assert gene_ID == fetch_correct_ID("TG3", "gene", cursor)
         assert match_strand == "-"
@@ -126,11 +139,12 @@ class TestSearchForOverlapWithGene(object):
         """ Example where query overlaps two genes. Must choose the one with 
             more overlap """
  
+        database = "scratch/toy.db"
         conn, cursor = get_db_cursor()
         build = "toy_build" 
-        talon.make_temp_novel_gene_table(cursor, "toy_build")
-        location_dict = talon.make_location_dict(build, cursor)
-        run_info = talon.init_run_info(cursor, build)
+        init_refs.make_temp_novel_gene_table(cursor, "toy_build")
+        location_dict = init_refs.make_location_dict(build, cursor)
+        run_info = talon.init_run_info(database, build)
 
         chrom = "chr1"
         pos = [800, 5050]
@@ -139,7 +153,8 @@ class TestSearchForOverlapWithGene(object):
         gene_ID, match_strand = talon.search_for_overlap_with_gene(chrom, pos[0],
                                                                    pos[1],
                                                                    strand, cursor,
-                                                                   run_info)
+                                                                   run_info,
+                                                                   "temp_gene")
 
         assert gene_ID == fetch_correct_ID("TG1", "gene", cursor)
         assert match_strand == "+"
