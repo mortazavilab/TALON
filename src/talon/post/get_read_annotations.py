22df0c46a0f5b3ef3954820168ea5fdcc510a463
Dana Elizabeth Wyman <dwyman@hpc-interactive-1-1.local>
2019-10-21 11:21:58 -0700

    Added some user safeguards to catch bad inputs

--- src/talon/post/get_read_annotations.py
+++ src/talon/post/get_read_annotations.py
@@ -6,6 +6,7 @@
 
 import argparse
 import sqlite3
+import os
 from .. import query_utils as qutils
 from string import Template
 
@@ -391,6 +392,31 @@ def make_read_annot_file(database, build, outprefix, datasets = "all"):
                                curr_ISM_novelty]) + "\n")
 
     o.close()
+    os.system("rm " + tmp_read_file)
+
+def check_build_validity(build, database):
+    """ Make sure that the user has entered a correct build name """
+
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name FROM genome_build")
+    builds = [str(x[0]) for x in cursor.fetchall()]
+    conn.close()
+
+    if build == None:
+        message = "Please provide a valid genome build name. " + \
+                  "In this database, your options are: " + \
+                  ", ".join(builds)
+        raise ValueError(message)
+
+    if build not in builds:
+        message = "Build name '" + build + \
+                  "' not found in this database. Try one of the following: " + \
+                  ", ".join(builds)
+        raise ValueError(message)
+
+    return
 
 def main():
     options = get_args()
@@ -398,6 +424,12 @@ def main():
     build = options.build
     datasets = options.datasets
     outprefix = options.outprefix
+
+    check_build_validity(build, database)
+    # Make sure that the input database exists!
+    if not Path(database).exists():
+        raise ValueError("Database file '%s' does not exist!" % database)
+
     if datasets != "all":
         datasets = datasets.split(",")   
    
