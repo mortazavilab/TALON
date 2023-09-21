0519f32d9681b26f455f5f8bbccf4f011e4902a0
fairliereese <fairliek@comcast.net>
2023-09-21 12:32:54 -0700

    added tests for option to filter known transcripts for all known, regardless of whether observed

--- testing_suite/filtering/test_fetch_known.py
+++ testing_suite/filtering/test_fetch_known.py
@@ -7,36 +7,55 @@ def test_get_known_transcripts_all_datasets():
         transcripts when datasets are not speicified """
 
     database = "scratch/filter/test.db"
-    known = filt.get_known_transcripts(database, "toy", datasets = None)
+    include_annot = False
+    known = filt.get_known_transcripts(database, "toy",
+                                       include_annot,
+                                       datasets = None)
+    assert list(known.gene_ID) == [1, 1]
+    assert list(known.transcript_ID) == [1, 2]
+
+def test_get_known_transcripts_dataset_1_include_annot():
+    """ Make sure the get_known_transcripts function returns all known
+        transcripts with the include_annot function """
+
+    database = "scratch/filter/test.db"
+    include_annot = False
+    known = filt.get_known_transcripts(database, "toy",
+                                       include_annot,
+                                       datasets = None)
     assert list(known.gene_ID) == [1, 1]
     assert list(known.transcript_ID) == [1, 2]
 
 def test_get_known_transcripts_specific_dataset():
-    """ Now make sure the correct transcript is returned when the dataset is 
+    """ Now make sure the correct transcript is returned when the dataset is
         specified. """
 
     database = "scratch/filter/test.db"
+    include_annot = False
 
     # Both datasets
-    known = filt.get_known_transcripts(database, "toy", 
+    known = filt.get_known_transcripts(database, "toy",
+                                       include_annot,
                                        datasets = ["dataset_1", "dataset_2"])
     assert list(known.gene_ID) == [1, 1]
     assert list(known.transcript_ID) == [1, 2]
 
     # Dataset 1
     known = filt.get_known_transcripts(database, "toy",
+                                       include_annot,
                                        datasets = ["dataset_1"])
     assert list(known.iloc[0]) == [1, 1]
     assert len(known) == 1
 
     # Dataset 2
     known = filt.get_known_transcripts(database, "toy",
+                                       include_annot,
                                        datasets = ["dataset_2"])
     assert list(known.iloc[0]) == [1, 2]
     assert len(known) == 1
 
     # Dataset 3
     known = filt.get_known_transcripts(database, "toy",
+                                       include_annot,
                                        datasets = ["dataset_3"])
     assert len(known) == 0
-
